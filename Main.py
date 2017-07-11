0040f6e1c8b24a73107c9693a529a46685b6a75f
u3shadow <u3shadow@gmail.com>
2017-07-11 16:00:30 +0800

    user can input score from command line

--- Main.py
+++ Main.py
@@ -1,19 +1,29 @@
 import  os
+import random
 import numpy as n
 import scipy.io as io
 
+def loadMovieList():
+    fid = open("movie_ids.txt")
+    n1 = 1682
+    movieList = [[] for i in range(n1)]
+    for i in range(0,n1):
+        line = fid.readline()
+        indx = line.index(" ")
+        idx = line[0:indx]
+        movieName = line[indx:]
+        movieList[i].append(movieName.strip())
+    fid.close()
+    return movieList
+
+movieList = loadMovieList()
 my_ratings = n.zeros((1682,1))
-my_ratings[1] = 4
-my_ratings[98] = 2
-my_ratings[7] = 3
-my_ratings[12]= 5
-my_ratings[54] = 4
-my_ratings[64]= 5
-my_ratings[66]= 3
-my_ratings[69] = 5
-my_ratings[183] = 4
-my_ratings[226] = 5
-my_ratings[355]= 5
+ran = range(1682)
+li = random.sample(ran,10)
+for i in range(0,10):
+    print "please input score for %s (1-5)"%movieList[li[i]]
+    s = input()
+    my_ratings[li[i]] = s
 ratting = {'my_ratings':my_ratings}
 io.savemat("my_ratting.mat",ratting)
 
