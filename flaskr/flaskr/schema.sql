80c8ce157087a872345ff67542927890e0b2c6cf
u3shadow <u3coding@gmail.com>
2017-07-30 19:53:20 +0800

    update init table scrip

--- flaskr/flaskr/schema.sql
+++ flaskr/flaskr/schema.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS `entries`;
 CREATE TABLE `entries`(
   `id` INT AUTO_INCREMENT,
   `title` TEXT NOT NULL,
-  `text` TEXT NOT NULL
+  `text` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
 );
