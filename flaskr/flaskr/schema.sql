9f1e49cce10a512bec9d2e116c69f50c59775380
u3shadow <u3coding@gmail.com>
2017-07-31 20:20:56 +0800

    add create user table sql

--- flaskr/flaskr/schema.sql
+++ flaskr/flaskr/schema.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS `entries`;
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `entries`(
   `id` INT AUTO_INCREMENT,
   `title` TEXT NOT NULL,
   `text` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+CREATE TABLE `users`(
+  `id` INT AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  `psw` TEXT NOT NULL,
+  `email` TEXT NOT NULL,
   PRIMARY KEY (`id`)
 );
