160eae4a885cf1ed2da9f0d4b264c5f782d27abb
root <root@vultr.guest>
2017-08-12 23:46:53 +0000

    add path to main

--- basecontent/MainToFlask.py
+++ basecontent/MainToFlask.py
@@ -1,4 +1,8 @@
 import  os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 import numpy as n
 import scipy.io as io
 from LoadGameFile import loadGameList
