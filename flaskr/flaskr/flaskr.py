422eff04c90cb1a37b3f30645cdc300eadc50c11
u3shadow <u3coding@gmail.com>
2017-08-13 08:04:38 +0800

    Update flaskr.py
    add path to flask

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1,5 +1,9 @@
 # -*- coding=UTF-8 -*-
 import os
+import sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 import random
 import uuid
 import json
